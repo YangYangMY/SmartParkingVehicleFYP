df7ef6cdd347e24206874d7f6a45ebfb0827fe1f
Lee Wee Yan <weeyanlee1231@gmail.com>
2023-09-26 15:27:36 +0800

    Update CarDetectController.py

--- ultralytics/CarDetection/CarDetectController.py
+++ ultralytics/CarDetection/CarDetectController.py
@@ -20,8 +20,8 @@ DOUBLEPARK_TIME = 30  # seconds to determine if a car is double park or not
 
 
 #Output Video Settings
-showResizedVideo = False
-showCombinedVideo = False #If this is enabled, showLeftCam & showMidCam & showRightCam won't work
+showResizedVideo = True
+showCombinedVideo = True #If this is enabled, showLeftCam & showMidCam & showRightCam won't work
 showLeftCam = True
 showMidCam = False
 showRightCam = False
@@ -29,17 +29,17 @@ showFPS = False
 
 #Video File Settings
 # Open the video file 1
-video_path1 = "../VideoFootage/rightCam4.mp4"
+video_path1 = "../VideoFootage/rightCam2.mp4"
 cap1 = cv2.VideoCapture(video_path1)
 mask1 = cv2.imread("MaskImage/maskRightCamera.png")
 
 # Open the video file 2
-video_path2 = "../VideoFootage/midCam4.mp4"
+video_path2 = "../VideoFootage/midCam2.mp4"
 cap2 = cv2.VideoCapture(video_path2)
 mask2 = cv2.imread("MaskImage/maskMiddleCamera.png")
 
 # Open the video file 3
-video_path3 = "../VideoFootage/leftCam4.mp4"
+video_path3 = "../VideoFootage/leftCam2.mp4"
 cap3 = cv2.VideoCapture(video_path3)
 mask3 = cv2.imread("MaskImage/maskLeftCamera.png")
 
